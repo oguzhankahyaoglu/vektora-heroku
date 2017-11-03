b33600090e1aacec616b07494f115ecb78db0d09
oğuzhan kahyaoğlu <oguzhan.kahyaoglu@yahoo.com>
2017-11-03 11:11:25 +0300

    Setup for Heroku

---
+++ BayiServiceTest/WebApplication1/Application/ServiceHelpers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Application
+{
+    [DebuggerStepThrough]
+    public static class ServiceCaller
+    {
+        [DebuggerStepThrough]
+        public static TResult CallVenüsBayi<TResult>(Func<BayiServices.ServiceClient, Task<TResult>> callMethod)
+        {
+            return DebugExec<BayiServices.ServiceClient, BayiServices.IService, TResult>(callMethod);
+        }
+
+        public static BayiServices.AuthInfo AuthToken => new BayiServices.AuthInfo { Username = "123", Password = "456" };
+
+        /// <summary>
+        /// Default olarak servisden 20 dk cevap bekler sonra timeout alır.
+        /// </summary>
+        private static TimeSpan DefaultTimeout = new TimeSpan(0, 20, 0);
+
+        [DebuggerStepThrough]
+        private static TResult DebugExec<TService, TChannel, TResult>(Func<TService, Task<TResult>> func)
+            where TService : ClientBase<TChannel>, new()
+            where TChannel : class
+        {
+            //ws.ChannelFactory.Endpoint.Binding.CloseTimeout
+            var ws = new TService();
+            if (ws.ChannelFactory?.Endpoint?.Binding == null)
+                return Task.Run(() => func(ws)).Result;
+
+            //maxReceivedMessageSize="20000000" maxBufferSize="20000000" maxBufferPoolSize="20000000"
+            var binding = ws.ChannelFactory.Endpoint.Binding;
+            var basicHttpBinding = binding as BasicHttpBinding;
+            if (basicHttpBinding != null)
+            {
+                var limit = 2 * 10000 * 1000;
+                basicHttpBinding.MaxBufferPoolSize = limit;
+                basicHttpBinding.MaxBufferSize = limit;
+                basicHttpBinding.MaxReceivedMessageSize = limit;
+                basicHttpBinding.AllowCookies = true;
+            }
+
+            if (binding.SendTimeout < DefaultTimeout)
+                binding.SendTimeout = DefaultTimeout;
+
+            if (binding.CloseTimeout < DefaultTimeout)
+                binding.CloseTimeout = DefaultTimeout;
+
+            if (binding.OpenTimeout < DefaultTimeout)
+                binding.OpenTimeout = DefaultTimeout;
+
+            if (binding.ReceiveTimeout < DefaultTimeout)
+                binding.ReceiveTimeout = DefaultTimeout;
+            return Task.Run(() => func(ws)).Result;
+        }
+
+    }
+}
---
+++ BayiServiceTest/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            var context = new BayiServices.ServiceClient();
+            var result = Task.Run(()=> context.GetCategoriesAsync(new BayiServices.AuthInfo { Username = "123", Password = "456" })).Result;
+            return View(result);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
