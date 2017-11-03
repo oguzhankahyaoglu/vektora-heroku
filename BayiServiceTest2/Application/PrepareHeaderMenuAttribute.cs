060f100a3c7e61970084423b914e01983159de7d
oğuzhan kahyaoğlu <oguzhan.kahyaoglu@yahoo.com>
2017-11-03 13:54:30 +0300

    üst menü bağlandı

---
+++ BayiServiceTest2/Application/PrepareHeaderMenuAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BayiServiceTest2.Application
+{
+    /// <summary>
+    /// Üst menüdeki kategori ağacını bağlar.
+    /// Ayrıca, birden fazla fiyat listesi seçip seçemeyeceğini set eder.
+    /// </summary>
+    public class PrepareHeaderMenuAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            //var selectedPriceList = Sessions.Ctx?.SelectedPriceList;
+            //if (filterContext.HttpContext.Request.IsAjaxRequest() || selectedPriceList == null)
+            //{
+            //    base.OnActionExecuted(filterContext);
+            //    return;
+            //}
+
+            if (filterContext.Result is ViewResult viewResult)
+            {
+                viewResult.ViewData["Categories"] = ServiceCaller.CallVenüsBayi(ws => ws.GetCategoriesAsync(ServiceCaller.AuthToken));
+            }
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
